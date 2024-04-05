7d5b3763bcb395f8bc43a6749584a26e6a169c60
Zan Lin <oaobj4@gmail.com>
2024-04-06 01:09:22 +0800

    first commit

---
+++ chat.py
@@ -0,0 +1,36 @@
+import os
+
+# 讀取文件檔，將文件檔內資料轉成list
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+# 將對話紀錄轉換成易讀的形式
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:
+            new.append(person + ': ' + line)
+    return new
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+
+def main():
+    lines = read_file('input.txt')
+    lines = convert(lines)
+    write_file('output.txt', lines)
+
+main()
